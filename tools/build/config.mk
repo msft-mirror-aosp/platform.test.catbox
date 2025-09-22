da77a640cb63b737f7b30d1704fcc9a8b61d541c
Naresh kumar Podishetty (xWF) <podishettyk@google.com>
2025-09-21 19:49:25 -0700

    Revert "Unflatten catbox to follow the same structure as other xts"
    This reverts commit 0c0230d1bcce4358eca932f76dee931671201252.
    
    Reason for revert: <Droid monitor created revert due to b/446536526. Will be verifying through ABTD before submission.>
    
    Fix : 446536526
    
    Change-Id: Ia12c3c9906c3b593fe235c881ee390db92c2d276

--- tools/build/config.mk
+++ tools/build/config.mk
@@ -13,4 +13,3 @@
 # limitations under the License.
 
 COMPATIBILITY_TESTCASES_OUT_catbox := $(HOST_OUT)/catbox/android-catbox/testcases
-COMPATIBILITY_TESTCASES_OUT_INCLUDE_MODULE_FOLDER_catbox := true
